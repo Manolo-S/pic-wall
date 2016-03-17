88ce2253485d0bf9422d16779980cc527ca032ee
Manolo Schotsborg <manolo.schotsborg@outlook.com>
2016-03-17 16:07:10 -0300

    add overlay with image title

--- pic-wall-responsive.js
+++ pic-wall-responsive.js
@@ -12,17 +12,47 @@ $grid.imagesLoaded().progress( function() {
 
 
 $('.add-button').on('click', function(event) {
-  var elems = [getItemElement()];
+  event.preventDefault();
+  var link = $("#link").val();
+  var title = $("#title").val();
+  var elems = [getItemElement(link, title)];
   // make jQuery object
   var $elems = $(elems);
   $grid.append($elems).masonry('appended', $elems);
+
+  // 1) Retrieve array [[title, link], [title, link], etc.] from database 
+  // 2) add new link and title to array and store array in database
 });
 
-// create <div class="grid-item"></div>
-function getItemElement() {
+
+$grid.on( 'click', '.grid-item', function( event ) {
+  event.preventDefault();
+  $grid.masonry( 'remove', event.currentTarget )
+    // trigger layout
+    .masonry();
+});
+
+
+function getItemElement(link, title) {
   var elem = document.createElement('div');
+  var image = document.createElement('img');
+  image.src = link;
+  var overlay = document.createElement('a');
+  overlay.className = "overlay";
+  overlay.href = "#";
+
+  var imageTitle = document.createElement('p');
+  imageTitle.className = "title";
+  var text = document.createTextNode(title);
+  imageTitle.appendChild(text);
+  overlay.appendChild(imageTitle);
+  elem.appendChild(image);
+  elem.appendChild(overlay);
   var hRand = Math.random();
   var heightClass = hRand > 0.85 ? 'grid-item--height4' : hRand > 0.6 ? 'grid-item--height3' : hRand > 0.35 ? 'grid-item--height2' : '';
-  elem.className = 'grid-item ' + heightClass;
+  elem.className = 'grid-item ' ;
   return elem;
 }
+
+
+

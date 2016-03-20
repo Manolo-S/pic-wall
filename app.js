6f96508d014214594f8cec627b87a694d2c56157
Manolo Schotsborg <manolo.schotsborg@outlook.com>
2016-03-20 17:05:37 -0300

    add image user to overlay 27

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(function(err, req, res, next) {
 app.set('port', process.env.PORT || 3000);
 
 
-// TODO: remove code below
+// // TODO: remove code below
 
 var picModel = require('./config/picModel');
 
